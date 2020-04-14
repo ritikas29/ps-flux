a35964fd572af950a2aae4198c00dcaff239b905
ritika29 <setritika16@gmail.com>
2020-04-14 11:43:41 +0530

    phases

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { render } from "react-dom";
+import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+ 
+render(<Router> < App/> </Router>,document.getElementById("root"));
